3e142d311208296c63cfead629697cf248ccad2e
tmagoudi <tmagoudi@f2r4s4.paris.42.school>
2026-09-15 10:36:04 +0200

    parsing

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,18 +1,32 @@
 import sys
-from parsing import parse
+from parsing import create_conf
+from typing import Optional
+import os
+from dotenv import load_dotenv
+
+
+def get_variable(name: str, default: Optional[str] = None) -> str:
+    value = os.getenv(name, default)
+    if value is None:
+        return "Missing"
+    return value
 
 def main() -> None:
     if len(sys.argv) != 2:
             print("Error: input should be \' python3 a_maze_ing.py config.txt \'")
             return
     try:
-        with open("config.txt", 'r') as file:
-            parse(file)
+        new_conf = create_conf()
+        new_conf.show_config()
     except Exception as e:
-         print(e)
+        print(e)
 
 if __name__ == "__main__":
+
+    main()
+    """
     data = ["hello"]
     exemple = ("hey", "hou","haha")
     data.extend(exemple)
     print(*data, sep= "\n")
+    """
--- parsing.py
+++ parsing.py
@@ -1,6 +1,57 @@
 from pydantic import BaseModel, Field
-import typing
+from dotenv import load_dotenv
+from typing import Annotated, Optional
+import os
 
-def parse(configfile: typing.IO[str]) -> None:
-    data: str = configfile.read()
-    print(data)
+class Config(BaseModel):
+    width: int = Field(ge=2, le=50)
+    height: int = Field(ge=2, le=50) 
+    entry:tuple[Cord, Cord]
+    exit: tuple[Cord, Cord]
+    o_file: str 
+    is_perfect: bool 
+    seed:Optional[int] = Field(default = None , ge = 0)
+
+    def show_config(self):
+        for value in self.model_dump().values():
+            print(value)
+
+
+def get_variable(name: str, default: Optional[str] = None) -> str:
+    value = os.getenv(name, default)
+    if value is None:
+        return "Missing"
+    return value
+
+load_dotenv()
+Cord = Annotated[int, Field(ge = 0, le = \
+    max(int(get_variable("WIDTH")) - 1, int(get_variable("HEIGHT")) -1 ))] 
+
+
+def create_conf() -> Config:
+    entry = get_variable("ENTRY").split(",")
+    exit = get_variable("EXIT").split(",")
+    seed = get_variable("SEED")
+
+    if entry == exit:
+        raise ValueError("The exit can't be at the same place as the entree !")
+
+    if seed != "Missing":
+        return Config(
+            width = int(get_variable("WIDTH")),
+            height = int(get_variable("HEIGHT")),
+            entry = (int(entry[0]), int(entry[1])),
+            exit = (int(exit[0]), int(exit[1])),
+            o_file = get_variable("OUTPUT_FILE"),
+            is_perfect= get_variable("PERFECT").lower() == "true",
+            seed = int(get_variable("SEED"))
+        )
+    else : 
+        return Config(
+                    width = int(get_variable("WIDTH")),
+                    height = int(get_variable("HEIGHT")),
+                    entry = (int(entry[0]), int(entry[1])),
+                    exit = (int(exit[0]), int(exit[1])),
+                    o_file = get_variable("OUTPUT_FILE"),
+                    is_perfect= get_variable("PERFECT").lower() == "true"
+                )
